b0718798c9ae7367d33f30bf776a793c8d9883c9
Nitronik16 <nitronik10@gmail.com>
2026-06-29 11:35:00 -0700

    more about stuff

--- personal-website/src/components/About.tsx
+++ personal-website/src/components/About.tsx
@@ -5,13 +5,12 @@ export default function About(){
 
     return(
         <>
-            <div>
-                <h1>About</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptas ea nobis rem. Dolores odit doloremque quasi quam natus iure, laborum, corrupti ratione doloribus sint tenetur ipsum nulla officiis harum!</p>
-            </div>
-            <div>
+            <h1 className='about-title'>About</h1>
+            <p className='about-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptas ea nobis rem. Dolores odit doloremque quasi quam natus iure, laborum, corrupti ratione doloribus sint tenetur ipsum nulla officiis harum!</p>
+            <div className='about-selector'>
                 
             </div>
         </>
+
     )
 }
